85f83bdeeee12f8ce9f6ac9b84d7f4c0d5a938d3
Fouad-Khelil <kf_khelil@esi.dz>
2024-04-26 12:35:43 +0200

    adding admin dashboard

---
+++ admin_dashboard/static/assets/js/pages/dashboard-default.js
@@ -0,0 +1,265 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function () {
+    floatchart();
+  }, 500);
+});
+
+function floatchart() {
+  (function () {
+    var options = {
+      chart: {
+        type: 'bar',
+        height: 90,
+        sparkline: {
+          enabled: true
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ['#FFF'],
+      stroke: {
+        curve: 'smooth',
+        width: 3
+      },
+      series: [
+        {
+          name: 'series1',
+          data: [45, 66, 41, 89, 25, 44, 9, 54]
+        }
+      ],
+      yaxis: {
+        min: 5,
+        max: 95
+      },
+      tooltip: {
+        theme: 'dark',
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return 'Total Earning';
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    };
+    var chart = new ApexCharts(document.querySelector('#tab-chart-1'), options);
+    chart.render();
+  })();
+  (function () {
+    var options = {
+      chart: {
+        type: 'area',
+        height: 90,
+        sparkline: {
+          enabled: true
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ['#FFF'],
+      stroke: {
+        curve: 'smooth',
+        width: 3
+      },
+      series: [
+        {
+          name: 'series1',
+          data: [35, 44, 9, 54, 45, 66, 41, 69]
+        }
+      ],
+      yaxis: {
+        min: 5,
+        max: 95
+      },
+      tooltip: {
+        theme: 'dark',
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return 'Total Earning';
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    };
+    var chart = new ApexCharts(document.querySelector('#tab-chart-2'), options);
+    chart.render();
+  })();
+  (function () {
+    var options = {
+      chart: {
+        type: 'bar',
+        height: 90,
+        sparkline: {
+          enabled: true
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ['#FFF'],
+      stroke: {
+        curve: 'smooth',
+        width: 3
+      },
+      series: [
+        {
+          name: 'series1',
+          data: [45, 66, 41, 89, 25, 44, 9, 54]
+        }
+      ],
+      yaxis: {
+        min: 5,
+        max: 95
+      },
+      tooltip: {
+        theme: 'dark',
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return 'Total Earning';
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    };
+    var chart = new ApexCharts(document.querySelector('#tab-chart-3'), options);
+    chart.render();
+  })();
+  (function () {
+    var options = {
+      chart: {
+        type: 'area',
+        height: 90,
+        sparkline: {
+          enabled: true
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ['#FFF'],
+      stroke: {
+        curve: 'smooth',
+        width: 3
+      },
+      series: [
+        {
+          name: 'series1',
+          data: [35, 44, 9, 54, 45, 66, 41, 69]
+        }
+      ],
+      yaxis: {
+        min: 5,
+        max: 95
+      },
+      tooltip: {
+        theme: 'dark',
+        fixed: {
+          enabled: false
+        },
+        x: {
+          show: false
+        },
+        y: {
+          title: {
+            formatter: function (seriesName) {
+              return 'Total Earning';
+            }
+          }
+        },
+        marker: {
+          show: false
+        }
+      }
+    };
+    var chart = new ApexCharts(document.querySelector('#tab-chart-4'), options);
+    chart.render();
+  })();
+  (function () {
+    var options = {
+      chart: {
+        type: 'area',
+        height: 480,
+        stacked: true,
+        toolbar: {
+          show: false
+        }
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '50%'
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ['#f36e21', '#2196f3'],
+      series: [
+        {
+          name: 'Loss',
+          data: [35, 125, 35, 35, 35, 80, 35, 20, 35, 45, 15, 75]
+        },
+        {
+          name: 'Profit',
+          data: [35, 15, 15, 35, 65, 40, 80, 25, 15, 85, 25, 75]
+        }
+      ],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: 'bottom',
+              offsetX: -10,
+              offsetY: 0
+            }
+          }
+        }
+      ],
+      xaxis: {
+        type: 'category',
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+      },
+      grid: {
+        strokeDashArray: 4
+      },
+      tooltip: {
+        theme: 'dark'
+      }
+    };
+    var chart = new ApexCharts(document.querySelector('#growthchart'), options);
+    chart.render();
+  })();
+}
